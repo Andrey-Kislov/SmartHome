611442d358d32a7d16f8df638c83625ff1883c2b
Andrey Kislov <andead@inbox.ru>
2020-04-24 16:15:07 +0300

    Added AttributeType

--- Andead.SmartHome.UnitOfWork/Entities/DeviceAttribute.cs
+++ Andead.SmartHome.UnitOfWork/Entities/DeviceAttribute.cs
@@ -1,9 +1,13 @@
-﻿namespace Andead.SmartHome.UnitOfWork.Entities
+﻿using Andead.SmartHome.UnitOfWork.Enums;
+
+namespace Andead.SmartHome.UnitOfWork.Entities
 {
     public class DeviceAttribute : Entity
     {
         public int DeviceId { get; set; }
 
         public string AttributeName { get; set; }
+
+        public AttributeType AttributeType { get; set; }
     }
 }
